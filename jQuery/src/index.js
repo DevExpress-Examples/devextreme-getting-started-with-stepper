2c83085d8d4ceac637d5baa7b063030939fbbe34
Vlada Skorokhodova <vlada.skorokhodova@devexpress.com>
2025-05-07 10:52:45 +0400

    Update for lint

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,40 +1,38 @@
-$(function() {
-  $("#stepper").dxStepper({
+$(() => {
+  $('#stepper').dxStepper({
     items: [{
-      label: "Personal Details",
-      template(data) {
-      return `<div class="star dx-step-indicator"></div>
-        <div class="dx-step-caption">
-          <div class="dx-step-label">${data.label}</div>
+      label: 'Personal Details',
+      template: (data) => {
+      return `<div class='star dx-step-indicator'></div>
+        <div class='dx-step-caption'>
+          <div class='dx-step-label'>${data.label}</div>
         </div>`;
       },
     }, {
-      label: "Program Selection",
-      icon: 'detailslayout'
+      label: 'Program Selection',
+      icon: 'detailslayout',
     }, {
-      label: "Campus and Start Dates",
-      icon: 'map'
+      label: 'Campus and Start Dates',
+      icon: 'map',
     }, {
-      label: "Supporting Documents",
-      icon: 'textdocument'
+      label: 'Supporting Documents',
+      icon: 'textdocument',
     }, {
-      label: "Scholarship and Aid",
+      label: 'Scholarship and Aid',
       icon: 'money',
       optional: true
     }, {
-      label: "Review and Submit",
-      icon: 'send'
+      label: 'Review and Submit',
+      icon: 'send',
     }],
     linear: true,
     selectedIndex: 0,
-    onSelectionChanged(e) {
+    onSelectionChanged: (e) => {
       const newItem = e.addedItems[0];
-      const items = e.component.option("items");
-      const newIndex = items.findIndex(item => newItem.label === item.label);
-      for (let i = 0; i < newIndex; i++) {
-        e.component.option(`items[${i}].disabled`, true);
-      }
+      const items = e.component.option('items');
+      const newIndex = items.findIndex((item) => newItem.label === item.label);
+      e.component.option(`items[${newIndex-1}].disabled`, true);
       return;
-    }
+    },
   });
 });
